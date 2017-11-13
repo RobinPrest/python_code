94f25272b571717ddf82f57854a57f3a4f6815f0
Gisnside <16131021+RobinPrest@users.noreply.github.com>
2017-11-13 18:50:47 +0100

    Form

--- Excel2CSV.py
+++ Excel2CSV.py
@@ -14,21 +14,23 @@ import_dir = input(u'Répertoire source :')
 export_dir = input(u'Répertoire cible (ex : X:\Data\):')
 count_read = 0
 
-#Fonction pour créer un fichier CSV à partir d'un fichier Excel
+
+# Fonction pour créer un fichier CSV à partir d'un fichier Excel
 def read_excel(excel_file, export_dir):
     sheet_to_df_map = {}
 
-    #Lecture de tous les noms de feuilles
+    # Lecture de tous les noms de feuilles
     xls = pd.ExcelFile(excel_file)
     shn = xls.sheet_names
 
-    #Boucle sur les noms de feuilles
+    # Boucle sur les noms de feuilles
     for sheet_name in shn:
-        df = pd.read_excel(excel_file,sheet_name)
-        #-->Possibilité ici de créer des modules de controle, modif ou autre
+        df = pd.read_excel(excel_file, sheet_name)
+        # -->Possibilité ici de créer des modules de controle, modif ou autre
 
-        #export en csv vers le répertoire spécifié
-        df.to_csv(export_dir+'_'+sheet_name+'.csv', sep=';', encoding='utf-8', index=False)
+        # export en csv vers le répertoire spécifié
+        df.to_csv(export_dir + '_' + sheet_name + '.csv', sep=';', encoding='utf-8', index=False)
+
 
 # Fonction pour chercher les fichier dans des repertoires et sous-repertoires
 def find_files(directory, pattern):
@@ -38,23 +40,24 @@ def find_files(directory, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
 
-#Lancement du process
+
+# Lancement du process
 try:
     print('Traitement en cours')
     exceptions = []
-    try:
-        for f in find_files(import_dir, '*.xls*'):
+    for f in find_files(import_dir, '*.xls*'):
+        try:
             f_path = os.path.dirname(f)
             f_basename = os.path.basename(f).split('.')[0]
-            read_excel(str(f), export_dir+str(f_basename))
-            print(f+' lu')
-            count_read = count_read+1
-    except Exception as e:
-        exceptions.append((str(f), e))
-        print(exceptions)
+            read_excel(str(f), export_dir + str(f_basename))
+            print(f + ' lu')
+            count_read = count_read + 1
+        except Exception as e:
+            exceptions.append((str(f), e))
+            print(exceptions)
 
-    print(str(count_read)+u' fichiers lus')
+    print(str(count_read) + u' fichiers lus')
 
-except:
+except Exception as e:
     print("Unexpected error:", sys.exc_info()[0])
     raise
